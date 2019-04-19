78cd38f9e9e224084d7a3fd3cfdd38804b769ca4
Melanie Sumner <melaniersumner@gmail.com>
2019-04-18 22:02:47 -0500

    adding actions

--- addon/components/modal-window.js
+++ addon/components/modal-window.js
@@ -5,27 +5,32 @@ export default Component.extend({
   layout,
   isModalVisible: false,
 
-  init() {
-    this._super(...arguments);
+  // add support for the ESC key
 
-    var index = this.get('index');
-    this.set('elementId', `component-id${index}`);
-  },
-  // willInsertElement() {
-  //   super.willInsertElement(...arguments);
-  //   this.set('lastFocus', document.activeElement);
-  // },
-
-  willDestroyElement() {
-    super.willDestroyElement(...arguments);
-    this.lastFocus.focus();
-  },
   actions: {
-    triggerModal() {
+    clickButton () {
+      // set the isModalVisible property to true
       this.set('isModalVisible', true);
+    },
+    triggerModal() {      
+      let promise = clickButton;
+      promise.then(() => {
+        // set tabindex to 0 on the modal
+        let modalWindow = document.querySelector(".a11y-modal");
+        modalWindow.setAttribute("tabindex", "0");
+        // set focus on the modal 
+        modalWindow.focus();
+      });     
+      
+      // add the modal-open class to the body element
+      let bodyElement = document.querySelector("body");
+      bodyElement.classList.add("modal-open");    
+
     },
     closeModal() {
       this.set('isModalVisible', false);
+      // set tabindex back to -1
+      // return the focus to the trigger button
     }
   }
 });
